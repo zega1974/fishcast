05936861aecd3c04e516a5b331647275ca3b747f
alessandro lemos <zegappte@gmail.com>
2026-06-12 20:18:59 -0300

    Adiciona rota preview da aba Minha Captura

---
+++ app/minha-captura-preview/page.tsx
@@ -0,0 +1,9 @@
+import MyCapturePanelPreview from "@/components/MyCapturePanelPreview";
+
+export default function MinhaCapturaPreviewPage() {
+  return (
+    <main className="min-h-screen bg-[#020711]">
+      <MyCapturePanelPreview />
+    </main>
+  );
+}
